3f65eb4cd2ac06ebb49cf46d796bbdf577469bcb
Max <max.anisimov230@gmail.com>
2016-06-17 23:23:46 +0300

    Changed main colors of the app. Added clear button to registration screen. Changed loading messages.

--- app/src/main/java/com/amaze_ing/mm/amazeandroid/RegisterActivity.java
+++ app/src/main/java/com/amaze_ing/mm/amazeandroid/RegisterActivity.java
@@ -3,6 +3,9 @@ package com.amaze_ing.mm.amazeandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -14,5 +17,19 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void register(View view) {
 
+    }
+
+    public void clear(View view) {
+        EditText username = (EditText) findViewById(R.id.r_username_txtfield);
+        EditText password = (EditText) findViewById(R.id.r_password_txtfield);
+        EditText email = (EditText) findViewById(R.id.r_email_txtfield);
+        EditText name = (EditText) findViewById(R.id.r_name_txtfield);
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio_buttons);
+
+        username.setText("");
+        password.setText("");
+        email.setText("");
+        name.setText("");
+        radioGroup.clearCheck();
     }
 }
